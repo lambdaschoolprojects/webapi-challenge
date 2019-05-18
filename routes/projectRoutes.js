086b5f6831c6dff7e6e884360273701d3ca6dc41
RaneWallin <rane@ranewallin.com>
2019-05-17 19:53:37 -0700

    GET /api/projects/:id/actions

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,54 +1,62 @@
-const express = require('express');
-const projectDB = require('../data/helpers/projectModel')
+const express = require("express");
+const projectDB = require("../data/helpers/projectModel");
 
 //middleware
-const { validateProjectId, validateProjectBody } = require('../middleware/projectMiddleware');
+const {
+  validateProjectId,
+  validateProjectBody
+} = require("../middleware/projectMiddleware");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const projects = await projectDB.get();
+router.get("/", async (req, res) => {
+  const projects = await projectDB.get();
 
-    res.json(projects);
+  res.json(projects);
 });
 
-router.get('/:id', validateProjectId, (req, res) => {
-    res.json(req.project);
+router.get("/:id", validateProjectId, (req, res) => {
+  res.json(req.project);
 });
 
-router.post('/', validateProjectBody, async (req, res) => {
-    try {
-        const project = await projectDB.insert(req.project);
-
-        res.status(200).json(project)
-    } catch(err) {
-        res.status(500).json(err);
-    }
+router.get("/:id/actions", validateProjectId, async (req, res) => {
+  res.json(req.project.actions);
 });
 
-router.put('/:id', validateProjectId, async (req, res) => {
-    try {
-        const project = await projectDB.update(req.project.id, req.body);
+router.post("/", validateProjectBody, async (req, res) => {
+  try {
+    const project = await projectDB.insert(req.project);
 
-        if(!project || Object.keys(project).length < 1) return res.status(400).json({ message: "Unable to update the project."});
-
-        res.status(200).json(project);
-    } catch(err) {
-        res.status(500).json(err);
-    }
-})
-
-router.delete('/:id', validateProjectId, async (req, res) => {
-    try {
-        const result = await projectDB.remove(req.project.id);
-
-        if(!result) return res.status(400).json({ message: "Could not delete the project."});
-
-        res.status(200).json(result);
-    } catch(err) {
-        res.status(500).json(err);
-    }
+    res.status(200).json(project);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+router.put("/:id", validateProjectId, async (req, res) => {
+  try {
+    const project = await projectDB.update(req.project.id, req.body);
+
+    if (!project || Object.keys(project).length < 1)
+      return res.status(400).json({ message: "Unable to update the project." });
+
+    res.status(200).json(project);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", validateProjectId, async (req, res) => {
+  try {
+    const result = await projectDB.remove(req.project.id);
+
+    if (!result)
+      return res.status(400).json({ message: "Could not delete the project." });
+
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
