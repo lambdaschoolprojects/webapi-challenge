5a0a0dc2048fe1ae0a116c2b7b2a8671fb96d3e9
RaneWallin <rane@ranewallin.com>
2019-05-17 18:44:29 -0700

    GET /api/projects/:1 and defined validateProjectId middleware

--- middleware/projectMiddleware.js
+++ middleware/projectMiddleware.js
@@ -1,5 +1,23 @@
 const projectDB = require('../data/helpers/projectModel')
 
-const validateProjectId = (req, res, next) => {
 
+
+const validateProjectId = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const project = await projectDB.get(id);
+
+        if (!project || Object.keys(project).length < 1) return res.status(400).json({ error: "No such project found."});
+
+        req.project = project;
+    } catch(err) {
+        res.status(500).json(err);
+    }
+
+    next();
+}
+
+module.exports = {
+    validateProjectId
 }
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const projectDB = require('../data/helpers/projectModel')
 
+//middleware
+const { validateProjectId } = require('../middleware/projectMiddleware');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     const projects = await projectDB.get();
 
     res.json(projects);
+})
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.json(req.project);
 })
 
 
