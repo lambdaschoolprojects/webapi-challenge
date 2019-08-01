d1a8ca37c6a90fa3778107d7fa346e9581439c78
Rane <github@blasph.me>
2019-07-31 19:04:17 -0700

    post merge cleanup

--- middleware/projectMiddleware.js
+++ middleware/projectMiddleware.js
@@ -1,34 +1,36 @@
-const projectDB = require('../data/helpers/projectModel')
-
-
+const projectDB = require("../data/helpers/projectModel");
 
 const validateProjectId = async (req, res, next) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    try {
-        const project = await projectDB.get(id);
+  try {
+    const project = await projectDB.get(id);
 
-        if (!project || Object.keys(project).length < 1) return res.status(400).json({ error: "No such project found."});
+    if (!project || Object.keys(project).length < 1)
+      return res.status(400).json({ error: "No such project found." });
 
-        req.project = project;
-    } catch(err) {
-        res.status(500).json(err);
-    }
+    req.project = project;
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
-    next();
-}
+  next();
+};
 
 const validateProjectBody = (req, res, next) => {
-    const { name, description } = req.body;
+  const { name, description } = req.body;
 
-    if (!name || !description) return res.status(400).json({ message: "Name and description are both required"});
+  if (!name || !description)
+    return res
+      .status(400)
+      .json({ message: "Name and description are both required" });
 
-    req.project = { name, description };
+  req.project = { name, description };
 
-    next();
-}
+  next();
+};
 
 module.exports = {
-    validateProjectId,
-    validateProjectBody
-}
+  validateProjectId,
+  validateProjectBody
+};
