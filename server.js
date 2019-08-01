09b2b9393c3c60eb8e9a02e94ab901c76c215665
Rane <github@blasph.me>
2019-07-31 19:03:33 -0700

    GET /api/projects/:id/actions

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
-const projectRouter = require('./routes/projectRoutes');
-const actionRouter = require('./routes/actionRoutes');
+const projectRouter = require("./routes/projectRoutes");
+const actionRouter = require("./routes/actionRoutes");
 
 const server = express();
 
-server.use(express.json());
-server.use('/api/projects', projectRouter);
-server.use('/api/actions', actionRouter);
+server.use(cors());
 
-server.get('/', (req, res) => {
-    res.send('<h1>Welcome to my server!</h1>')
+server.use(express.json());
+server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
+
+server.get("/", (req, res) => {
+  res.send("<h1>Welcome to my server!</h1>");
 });
 
 module.exports = server;
